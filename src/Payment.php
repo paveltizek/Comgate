a951ed4fffb09adef5d2661f7c3aa369376545b1
Pavel Tížek <pavel@tizek.cz>
2018-06-19 10:20:23 +0200

    Added setters and getters for options

--- src/Payment.php
+++ src/Payment.php
@@ -26,6 +26,36 @@ class Payment extends Object
      * Payment constructor.
      * @param Service $service
      */
+    private $vatPL;
+
+    private $category;
+
+    private $label;
+
+    private $payerId;
+
+    private $method;
+
+    private $account;
+
+    private $email;
+
+    private $phone;
+
+    private $productName;
+
+    private $language;
+
+    private $reccurring;
+
+    private $reccurringId;
+
+    private $eetReport;
+
+    private $eetData;
+
+    private $country;
+
     public function __construct(Service $service)
     {
         if (!file_exists(__DIR__ . "/data")) {
@@ -44,6 +74,22 @@ class Payment extends Object
             $this->service->getSandbox(),
             $this->service->getSecret()
         );
+
+        $this->vatPL = "STANDARD";
+        $this->category = "PHYSICAL";
+        $this->label = "payment";
+        $this->payerId = null;
+        $this->method = "ALL";
+        $this->account = "";
+        $this->email = "";
+        $this->phone = "";
+        $this->productName = "";
+        $this->language = "";
+        $this->reccurring = false;
+        $this->reccurringId = null;
+        $this->eetReport = false;
+        $this->eetData = null;
+        $this->country = "CZ";
     }
 
     /**
@@ -55,26 +101,27 @@ class Payment extends Object
         $this->refId = $this->paymentsDatabase->createNextRefId();
         $this->price = $price;
 
+
         $this->paymentsProtocol->createTransaction(
-            "CZ",                                               // country
+            $this->country,                                               // country
             $price,                                             // price
             $this->service->getCurrency(),                      // currency
-            "payment",                                          // description
+            $this->label,                                          // description
             $this->refId,                                       // refId
-            null,                                               // payerId
-            "STANDARD",                                         // vatPL
-            "PHYSICAL",                                         // category
-            "ALL",                                              // method
-            "",                                                 // account
-            "",                                                 // email
-            "",                                                 // phone
-            "",                                                 // productName
-            "",                                                 // language
+            $this->payerId,                                               // payerId
+            $this->vatPL,                                         // vatPL
+            $this->category,                                         // category
+            $this->method,                                              // method
+            $this->account,                                                 // account
+            $this->email,                                                 // email
+            $this->phone,                                                 // phone
+            $this->productName,                                                 // productName
+            $this->language,                                                 // language
             $this->service->getPreauth(),                       // preauth
-            false,                                              // reccuring
-            null,                                               // reccuringId
-            false,                                              // eetReport
-            null                                                // eetData
+            $this->reccurring,                                              // reccuring
+            $this->reccurringId,                                               // reccuringId
+            $this->eetReport,                                              // eetReport
+            $this->eetData                                                // eetData
         );
     }
 
@@ -104,4 +151,326 @@ class Payment extends Object
     {
         return $this->paymentsProtocol->getTransactionId();
     }
+
+    /**
+     * @return Service
+     */
+    public function getService() {
+        return $this->service;
+    }
+
+    /**
+     * @param Service $service
+     * @return Payment
+     */
+    public function setService($service) {
+        $this->service = $service;
+        return $this;
+    }
+
+    /**
+     * @return \AgmoPaymentsSimpleDatabase
+     */
+    public function getPaymentsDatabase() {
+        return $this->paymentsDatabase;
+    }
+
+    /**
+     * @param \AgmoPaymentsSimpleDatabase $paymentsDatabase
+     * @return Payment
+     */
+    public function setPaymentsDatabase($paymentsDatabase) {
+        $this->paymentsDatabase = $paymentsDatabase;
+        return $this;
+    }
+
+    /**
+     * @return \AgmoPaymentsSimpleProtocol
+     */
+    public function getPaymentsProtocol() {
+        return $this->paymentsProtocol;
+    }
+
+    /**
+     * @param \AgmoPaymentsSimpleProtocol $paymentsProtocol
+     * @return Payment
+     */
+    public function setPaymentsProtocol($paymentsProtocol) {
+        $this->paymentsProtocol = $paymentsProtocol;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRefId() {
+        return $this->refId;
+    }
+
+    /**
+     * @param string $refId
+     * @return Payment
+     */
+    public function setRefId($refId) {
+        $this->refId = $refId;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice() {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     * @return Payment
+     */
+    public function setPrice($price) {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVatPL() {
+        return $this->vatPL;
+    }
+
+    /**
+     * @param mixed $vatPL
+     * @return Payment
+     */
+    public function setVatPL($vatPL) {
+        $this->vatPL = $vatPL;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory() {
+        return $this->category;
+    }
+
+    /**
+     * @param string $category
+     * @return Payment
+     */
+    public function setCategory($category) {
+        $this->category = $category;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel() {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     * @return Payment
+     */
+    public function setLabel($label) {
+        $this->label = $label;
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getPayerId() {
+        return $this->payerId;
+    }
+
+    /**
+     * @param null $payerId
+     * @return Payment
+     */
+    public function setPayerId($payerId) {
+        $this->payerId = $payerId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod() {
+        return $this->method;
+    }
+
+    /**
+     * @param string $method
+     * @return Payment
+     */
+    public function setMethod($method) {
+        $this->method = $method;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccount() {
+        return $this->account;
+    }
+
+    /**
+     * @param string $account
+     * @return Payment
+     */
+    public function setAccount($account) {
+        $this->account = $account;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail() {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return Payment
+     */
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone() {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return Payment
+     */
+    public function setPhone($phone) {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProductName() {
+        return $this->productName;
+    }
+
+    /**
+     * @param string $productName
+     * @return Payment
+     */
+    public function setProductName($productName) {
+        $this->productName = $productName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage() {
+        return $this->language;
+    }
+
+    /**
+     * @param string $language
+     * @return Payment
+     */
+    public function setLanguage($language) {
+        $this->language = $language;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReccurring() {
+        return $this->reccurring;
+    }
+
+    /**
+     * @param bool $reccurring
+     * @return Payment
+     */
+    public function setReccurring($reccurring) {
+        $this->reccurring = $reccurring;
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getReccurringId() {
+        return $this->reccurringId;
+    }
+
+    /**
+     * @param null $reccurringId
+     * @return Payment
+     */
+    public function setReccurringId($reccurringId) {
+        $this->reccurringId = $reccurringId;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEetReport() {
+        return $this->eetReport;
+    }
+
+    /**
+     * @param bool $eetReport
+     * @return Payment
+     */
+    public function setEetReport($eetReport) {
+        $this->eetReport = $eetReport;
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getEetData() {
+        return $this->eetData;
+    }
+
+    /**
+     * @param null $eetData
+     * @return Payment
+     */
+    public function setEetData($eetData) {
+        $this->eetData = $eetData;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry() {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     * @return Payment
+     */
+    public function setCountry($country) {
+        $this->country = $country;
+        return $this;
+    }
+
+
 }
